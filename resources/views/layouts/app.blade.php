06981c00126f74116a556be18d9c280774d21820
yu05 <orange60250@gmail.com>
2019-11-16 14:21:49 +0000

    CRUD tasks

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,16 +12,12 @@
          @include('commons.navbar')
         
         <div class="container">
+            @include('commons.error_messages')
+            
             @yield('content')
         </div>
         
         <header class="mb-4">
-            <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
-                <a class="navbar-brand" href="/">TaskList</a>
-                
-                <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
                 
                 <div class="collapse navbar-collapse" id="nav-bar">
                     <ul class="navbar-nav mr-auto"></ul>
@@ -29,12 +25,7 @@
                         <li class="nav-item">{!! link_to_route('tasks.create', '新規メッセージの投稿', [], ['class' => 'nav-link']) !!}</li>
                     </ul>
                 </div>
-            </nav>
         </header>
-        
-        <div class="container">
-            @yield('content')
-        </div>
         
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-    <h1>id = {{ $message->id }} のメッセージ詳細ページ</h1>
+    <h1>id = {{ $task->id }} のメッセージ詳細ページ</h1>
 
     <table class="table table-bordered">
         <tr>
