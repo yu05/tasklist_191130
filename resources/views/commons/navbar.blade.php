7f96798109db0445b6dc5aa6487c73996644fcbb
yu05 <orange60250@gmail.com>
2019-11-21 14:24:26 +0000

    tasklist-top page

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -9,6 +9,7 @@
         <div class="collapse navbar-collapse" id="nav-bar">
             <ul class="navbar-nav mr-auto"></ul>
             <ul class="navbar-nav">
+                <li class="nav-item"><a href="#" class="nav-link">Login</a></li>
                 <li class="nav-item">{!! link_to_route('tasks.create', '新規メッセージの投稿', [], ['class' => 'nav-link']) !!}</li>
             </ul>
         </div>
